c55d41767c294009ebbf4f1798096b4168626007
Shi_Sheng <39020034+ShiSheng233@users.noreply.github.com>
2020-10-02 22:08:49 +0800

    First commit with code

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace _1s
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            
+        }
+
+        public static long click_times = 0;
+
+        private void plus_one_second_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (click_times != -1)
+            {
+
+                click_times++;
+                show_click_times.Text = click_times.ToString() + "s";
+
+            }
+
+            
+        }
+    }
+}
